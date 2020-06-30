60a31af3eca67afddee8d4e6432afe4354a554b8
ARIAS <AMArias@smart.com.ph>
2020-06-30 11:22:39 +0800

    fixed layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,12 +1,32 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <q-header elevated reveal reveal-offset="100">
       <q-toolbar>
         <q-toolbar-title>
-          Quasar App
+          ESPortal
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn round flat icon="mdi-newspaper">
+          <q-tooltip content-class="text-uppercase">advisories</q-tooltip>
+        </q-btn>
+
+        <q-btn round flat icon="mdi-apps">
+          <q-tooltip content-class="text-uppercase">
+            exclusive selections
+          </q-tooltip>
+        </q-btn>
+
+        <q-btn round flat icon="mdi-calendar">
+          <q-tooltip content-class="text-uppercase">extra sccop</q-tooltip>
+        </q-btn>
+
+        <q-btn round flat icon="mdi-server-network">
+          <q-tooltip content-class="text-uppercase">enabling systems</q-tooltip>
+        </q-btn>
+
+        <q-separator vertical inset dark spaced />
+
+        <q-btn flat label="esolve" />
       </q-toolbar>
     </q-header>
 
